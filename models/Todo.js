84fbc494268872292cbf39a8f3ef3989baed2e96
Maxwell Stout <mastout252@gmail.com>
2022-09-03 19:14:57 -0400

    updated model to add listName

--- models/Todo.js
+++ models/Todo.js
@@ -1,18 +1,23 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const TodoSchema = new mongoose.Schema({
-  todo: {
-    type: String,
-    required: true,
-  },
-  completed: {
-    type: Boolean,
-    required: true,
-  },
-  userId: {
-    type: String,
-    required: true
-  }
-})
+	todo: {
+		type: String,
+		required: true
+	},
+	completed: {
+		type: Boolean,
+		required: true
+	},
+	userId: {
+		type: String,
+		required: true
+	},
+	listName: {
+		type: String,
+		required: true,
+		default: 'uncategorized'
+	}
+});
 
-module.exports = mongoose.model('Todo', TodoSchema)
+module.exports = mongoose.model('Todo', TodoSchema);
